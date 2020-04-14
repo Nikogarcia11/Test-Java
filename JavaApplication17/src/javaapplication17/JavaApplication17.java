a10299ac878da24271d7d95c76436b213aaaa779
Nicolas Garcia <agarcia1120@gmail.com>
2020-04-14 17:57:28 -0500

    Todo esta funcionando aparentemente bien

---
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,12 @@
+package javaapplication17;
+
+public class JavaApplication17 
+{
+ 
+    public static void main(String[] args)
+    {
+        System.out.println("Todo esta funcionando de maravilla");
+        System.out.println("Este es el segundo mensaje");
+    }
+    
+}
