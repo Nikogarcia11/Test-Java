62da406a8c0de9d4073636b71ca7081475d294d6
Nicolas Garcia <agarcia1120@gmail.com>
2020-04-14 18:13:42 -0500

    Clase multiplicacion y funcion multiplicar

--- JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -8,9 +8,14 @@ public class JavaApplication17
         System.out.println("Todo esta funcionando de maravilla");
         System.out.println("Este es el segundo mensaje");
         Suma sum=new Suma();
+        Multiplicacion multi=new Multiplicacion();
         int a=10,b=35;
         int c=sum.sumar(a, b);
         System.out.println("Suma es :"+c);
+        //Vamos a hacer una funcion que multiplique
+        float d=5f,e=15f;
+        float g=multi.multiplicar(d, e);
+        System.out.println("Multiplicacion :"+g);
     }
     
 }
