b599127158f75660cab2960ea01ffc1fb03750fe
Nicolas Garcia <agarcia1120@gmail.com>
2020-04-15 12:34:03 -0500

    Vida bella 3

--- JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -20,7 +20,6 @@ public class JavaApplication17
         System.out.println("La vida es bella1");
         System.out.println("La vida es bella 2");
         System.out.println("La vida es bella 3");
-        System.out.println("VIda bella 4");
     }
     
 }
