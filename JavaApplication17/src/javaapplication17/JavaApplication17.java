77f536bee4b42593d415c8ae339dc8b1d3b401fc
Nicolas Garcia <agarcia1120@gmail.com>
2020-04-15 10:21:47 -0500

    La vida es bella 3

--- JavaApplication17/src/javaapplication17/JavaApplication17.java
+++ JavaApplication17/src/javaapplication17/JavaApplication17.java
@@ -19,6 +19,7 @@ public class JavaApplication17
         System.out.println("Esta es una actualizacion de prueba");
         System.out.println("La vida es bella1");
         System.out.println("La vida es bella 2");
+        System.out.println("La vida es bella 3");
     }
     
 }
